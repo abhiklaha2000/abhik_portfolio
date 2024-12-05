0e6f4cc4baf0a8b70cfafaaf0d320db07e65a7b1
Sajal Ghosh <sajalghosh.itrd@gmail.com>
2024-12-05 09:25:53 +0530

    complete

---
+++ src/data/Education.js
@@ -0,0 +1,20 @@
+export const educations = [
+  {
+    id: 1,
+    title: "Bachelor Degree",
+    duration: "2019 - 2023",
+    institution: "Government College of Engineering and Textile Technology",
+  },
+  {
+    id: 2,
+    title: "Higher Secondary",
+    duration: "2016 - 2018",
+    institution: "Kulti High School (H/S)",
+  },
+  {
+    id: 3,
+    title: "Secondary ",
+    duration: "2015 - 2016",
+    institution: "Kulti High School (H/S)",
+  }
+]
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import About from '../components/About'
+
+import ContactSection from '../components/ContactSection'
+import Education from '../components/Education'
 import Experience from '../components/Experience'
 import HeroSection from '../components/HeroSection'
+import Projects from '../components/Projects'
+import Skills from '../components/Skills'
+import Footer from "../components/Footer"
 
 
 const Home = () => {
@@ -10,6 +16,11 @@ const Home = () => {
             <HeroSection/>
             <About/>
             <Experience/>
+            <Skills/>
+            <Projects/>
+            <Education/>
+            <ContactSection/>
+            <Footer/>
         </div>
     )
 }
